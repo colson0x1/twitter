843eaa4d44363fcf1ac3a4524d7c8e5325d94c72
Colson Thapa <colson@Colsons-MacBook-Pro.local>
2022-03-12 13:51:56 +0545

    initialized templating engine

--- server.js
+++ server.js
@@ -2,6 +2,16 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.set("view engine", "pug");
+app.set("views", "views");
+
 const server = app.listen(port, (req, res) => {
     console.log(`server running on port ${port}`);
+})
+
+app.get('/', (req, res) => {
+    let payload = {
+        pageTitle: "Home"
+    }
+    res.status(200).render("home", payload);
 })
