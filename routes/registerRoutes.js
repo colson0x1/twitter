52c6e09411572f07666be0b5e1d52508c1474acd
Colson Thapa <colson@Colsons-MacBook-Pro.local>
2022-03-12 19:25:53 +0545

    added timestamp and encrypted password

--- routes/registerRoutes.js
+++ routes/registerRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const router = express.Router();
 const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt');
 const User = require('../schemas/UserSchema');
 
 app.set("view engine", "pug");
@@ -39,6 +40,8 @@ router.post('/', async (req, res) => {
         if (user == null) {
             // no user found
             let data = req.body;
+
+            data.password = await bcrypt.hash(password, 10);
 
             User.create(data)
             .then((user) => {
