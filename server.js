fb02eacaff55d599a7ed22f2ed88a3bd237a65c3
Colson Thapa <colson@Colsons-MacBook-Pro.local>
2022-03-12 14:49:43 +0545

    created login

--- server.js
+++ server.js
@@ -2,14 +2,22 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const middleware = require('./middleware');
+
 app.set("view engine", "pug");
 app.set("views", "views");
+
+// @routes
+const loginRoute = require('./routes/loginRoutes');
+
+app.use('/login', loginRoute);
+
 
 const server = app.listen(port, (req, res) => {
     console.log(`server running on port ${port}`);
 })
 
-app.get('/', (req, res) => {
+app.get('/', middleware.requireLogin, (req, res) => {
     let payload = {
         pageTitle: "Home"
     }
